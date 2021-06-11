5cb7e15a08a02ca1b6bf062ff1eebc0bdaee1e7f
James Hunt <jim@thetwopercent.co.uk>
2021-06-11 12:11:08 +0700

    Adding CN text to highlights block

--- lf-blocks/src/case-study-highlights/render-callback.php
+++ lf-blocks/src/case-study-highlights/render-callback.php
@@ -17,12 +17,18 @@ function lf_case_study_highlights_render_callback( $attributes ) {
 	// get the classes set from the block if any.
 	$classes = isset( $attributes['className'] ) ? $attributes['className'] : '';
 	// get content.
-	$heading_text01 = isset( $attributes['headingText01'] ) ? $attributes['headingText01'] : '';
-	$heading_text02 = isset( $attributes['headingText02'] ) ? $attributes['headingText02'] : '';
-	$heading_text03 = isset( $attributes['headingText03'] ) ? $attributes['headingText03'] : '';
+	$heading_text01   = isset( $attributes['headingText01'] ) ? $attributes['headingText01'] : '';
+	$heading_text02   = isset( $attributes['headingText02'] ) ? $attributes['headingText02'] : '';
+	$heading_text03   = isset( $attributes['headingText03'] ) ? $attributes['headingText03'] : '';
 	$highlight_stat01 = isset( $attributes['smallerText01'] ) ? $attributes['smallerText01'] : '';
 	$highlight_stat02 = isset( $attributes['smallerText02'] ) ? $attributes['smallerText02'] : '';
 	$highlight_stat03 = isset( $attributes['smallerText03'] ) ? $attributes['smallerText03'] : '';
+
+	if ( is_singular( 'lf_case_study_cn' ) ) {
+		$highlights_title = '一些数据...';
+	} else {
+		$highlights_title = 'By the numbers...';
+	}
 
 	ob_start();
 	?>
@@ -30,7 +36,9 @@ function lf_case_study_highlights_render_callback( $attributes ) {
 
 <div class="case-study-highlights">
 <div class="container">
-<h2 class="case-study-highlights-title">By the numbers...</h2>
+	<?php if ( $highlights_title ) : ?>
+<h2 class="case-study-highlights-title"><?php echo esc_html( $highlights_title ); ?></h2>
+	<?php endif; ?>
 <div class="case-study-highlights-wrapper">
 <div class="case-study-highlight-text">
 	<?php if ( $heading_text01 ) : ?>
@@ -63,8 +71,8 @@ endif;
 <?php endif; ?>
 </div>
 </div>
-</div></div>
-
+</div>
+</div>
 </section>
 	<?php
 	$block_content = ob_get_clean();
