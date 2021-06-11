8df0a7ae62ebc2d4da0138207f60bc86753c7ff8
James Hunt <jim@thetwopercent.co.uk>
2021-06-11 15:54:48 +0700

    Updating case study overview

--- lf-blocks/src/case-study-overview/render-callback.php
+++ lf-blocks/src/case-study-overview/render-callback.php
@@ -16,13 +16,13 @@
  */
 function lf_case_study_overview_render_callback( $attributes, $content ) {
 
-	// get the classes set from the block if any.
-	$classes = isset( $attributes['className'] ) ? $attributes['className'] : '';
-
 	// not on a case study page.
 	if ( ! is_singular( array( 'lf_case_study', 'lf_case_study_cn' ) ) ) {
 		return;
 	}
+
+	// get the classes set from the block if any.
+	$classes = isset( $attributes['className'] ) ? $attributes['className'] : '';
 
 	if ( is_singular( 'lf_case_study_cn' ) ) {
 		// get chinese content.
@@ -31,7 +31,7 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 			$industry->name = preg_replace( '/(.+)(\(\D+\))/', '$1', $industry->name );
 		}
 
-		$location = preg_replace( '/(.+)(\(\D+\))/', '$1', Lf_Utils::get_term_names( get_the_ID(), 'lf-country-cn', true ) );
+		$location      = preg_replace( '/(.+)(\(\D+\))/', '$1', Lf_Utils::get_term_names( get_the_ID(), 'lf-country-cn', true ) );
 		$location_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-country-cn', true );
 
 		$cloud_types = get_the_terms( get_the_ID(), 'lf-cloud-type-cn' );
@@ -39,7 +39,7 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 			$cloud_type->name = preg_replace( '/(.+)(\(\D+\))/', '$1', $cloud_type->name );
 		}
 
-		$product_type = preg_replace( '/(.+)(\(\D+\))/', '$1', Lf_Utils::get_term_names( get_the_ID(), 'lf-product-type-cn', true ) );
+		$product_type      = preg_replace( '/(.+)(\(\D+\))/', '$1', Lf_Utils::get_term_names( get_the_ID(), 'lf-product-type-cn', true ) );
 		$product_type_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-product-type-cn', true );
 
 		$challenges = get_the_terms( get_the_ID(), 'lf-challenge-cn' );
@@ -47,7 +47,7 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 			$challenge->name = preg_replace( '/(.+)(\(\D+\))/', '$1', $challenge->name );
 		}
 
-		$company_text      = '公司';
+		$organisation_text      = '组织';
 		$industry_text     = '行业';
 		$location_text     = '地点';
 		$cloud_type_text   = '云类型';
@@ -57,22 +57,24 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 
 		$url_type = '-cn';
 
+		$company_logo = get_post_meta( get_the_ID(), 'lf_case_study_cn_company_logo', true );
+
 	} else {
 
 		// get english content.
 		$industries = get_the_terms( get_the_ID(), 'lf-industry' );
 
-		$location = Lf_Utils::get_term_names( get_the_ID(), 'lf-country', true );
+		$location      = Lf_Utils::get_term_names( get_the_ID(), 'lf-country', true );
 		$location_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-country', true );
 
 		$cloud_types = get_the_terms( get_the_ID(), 'lf-cloud-type' );
 
-		$product_type = Lf_Utils::get_term_names( get_the_ID(), 'lf-product-type', true );
+		$product_type      = Lf_Utils::get_term_names( get_the_ID(), 'lf-product-type', true );
 		$product_type_slug = Lf_Utils::get_term_slugs( get_the_ID(), 'lf-product-type', true );
 
 		$challenges = get_the_terms( get_the_ID(), 'lf-challenge' );
 
-		$company_text      = 'Company';
+		$organisation_text = 'Organization';
 		$industry_text     = 'Industry';
 		$location_text     = 'Location';
 		$cloud_type_text   = 'Cloud Type';
@@ -81,7 +83,12 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 		$date_published    = 'Published';
 
 		$url_type = '';
+
+		$company_logo = get_post_meta( get_the_ID(), 'lf_case_study_company_logo', true );
+
 	}
+
+	$projects = get_the_terms( get_the_ID(), 'lf-project' );
 
 	ob_start();
 	?>
@@ -90,76 +97,127 @@ function lf_case_study_overview_render_callback( $attributes, $content ) {
 
 	<div class="case-study-overview">
 
+		<!-- column 1 -->
 		<div class="case-study-intro-wrapper">
-		<?php echo wp_kses_post( $content ); ?>
+			<?php echo wp_kses_post( $content ); ?>
 		</div>
 
-		<div class=" case-study-overview-wrapper">
+		<!-- column 2 -->
+		<div class="case-study-overview-wrapper">
 
-		<div>
-				<p><?php echo esc_html( $company_text ); ?></p>
-				<a class="skew-box secondary" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-			</div>
+<!-- organisation block -->
+			<div>
 
-			<?php
+				<h5 class="margin-bottom"><?php echo esc_html( $organisation_text ); ?></h5>
+
+<?php if ( $company_logo ) {
+	LF_Utils::display_responsive_images( $company_logo, 'spotlight-320', '200px', 'case-study-company-logo' );
+	} else { ?>
+	<h3><?php the_title(); ?></h3>
+<?php }
+
 			if ( ! empty( $challenges ) && ! is_wp_error( $challenges ) ) :
-				?>
-			<div>
-				<p><?php echo esc_html( $challenge_text ); ?></p>
-				<?php foreach ( $challenges as $challenge ) { ?>
-				<a class="skew-box secondary" title="See more case studies with a <?php echo esc_attr( $challenge->name ); ?> challenge" href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-challenge<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $challenge->slug ); ?>"><?php echo esc_html( $challenge->name ); ?></a>
-				<?php } ?>
-			</div>
-				<?php
-			endif;
 
-			if ( ! empty( $industries ) && ! is_wp_error( $industries ) ) :
+				$number_of_items = count( $challenges );
+				$i = 0;
 				?>
-			<div>
-				<p><?php echo esc_html( $industry_text ); ?></p>
-				<?php foreach ( $industries as $industry ) { ?>
-				<a class="skew-box secondary" title="See more case studies from <?php echo esc_attr( $industry->name ); ?>" href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-industry<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $industry->slug ); ?>"><?php echo esc_html( $industry->name ); ?></a>
-				<?php } ?>
-			</div>
+				<p><span class="strong"><?php echo esc_html( $challenge_text ); ?>:</span>
+				<?php foreach ( $challenges as $challenge ) { ?>
+				<a
+					title="See more case studies with a <?php echo esc_attr( $challenge->name ); ?> challenge"
+					href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-challenge<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $challenge->slug ); ?>"><?php echo esc_html( $challenge->name ); ?></a><?php
+			if ( ++$i < $number_of_items ) echo ', ';
+			} ?>
+				</p>
 				<?php
 				endif;
 
+			if ( ! empty( $industries ) && ! is_wp_error( $industries ) ) :
+				?>
+				<p><span class="strong"><?php echo esc_html( $industry_text ); ?>:</span>
+				<?php foreach ( $industries as $industry ) { ?>
+				<a
+					title="See more case studies from <?php echo esc_attr( $industry->name ); ?>"
+					href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-industry<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $industry->slug ); ?>"><?php echo esc_html( $industry->name ); ?></a>
+				<?php } ?>
+			</p>
+				<?php
+			endif;
+
 			if ( ! empty( $location ) && ! is_wp_error( $location ) ) :
 				?>
-			<div>
-				<p><?php echo esc_html( $location_text ); ?></p>
-				<a class="skew-box secondary" title="See more case studies from <?php echo esc_attr( $location ); ?>" href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-country<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $location_slug ); ?>"><?php echo esc_html( $location ); ?></a>
-			</div>
+				<p><span class="strong"><?php echo esc_html( $location_text ); ?>:</span>
+				<a
+					title="See more case studies from <?php echo esc_attr( $location ); ?>"
+					href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-country<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $location_slug ); ?>"><?php echo esc_html( $location ); ?></a>
+</p>
 				<?php
-					endif;
+endif;
 
 			if ( ! empty( $cloud_types ) && ! is_wp_error( $cloud_types ) ) :
 				?>
-			<div>
-				<p><?php echo esc_html( $cloud_type_text ); ?></p>
+				<p><span class="strong"><?php echo esc_html( $cloud_type_text ); ?>:</span>
 				<?php foreach ( $cloud_types as $cloud_type ) { ?>
-				<a class="skew-box secondary" title="See more case studies with a <?php echo esc_attr( $cloud_type->name ); ?> cloud type" href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-cloud-type<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $cloud_type->slug ); ?>"><?php echo esc_html( $cloud_type->name ); ?></a>
+				<a
+					title="See more case studies with a <?php echo esc_attr( $cloud_type->name ); ?> cloud type"
+					href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-cloud-type<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $cloud_type->slug ); ?>"><?php echo esc_html( $cloud_type->name ); ?></a>
 				<?php } ?>
-			</div>
+</p>
 				<?php
-					endif;
+endif;
 
 			if ( ! empty( $product_type ) && ! is_wp_error( $product_type ) ) :
 				?>
-			<div>
-				<p><?php echo esc_html( $product_type_text ); ?></p>
-				<a class="skew-box secondary" title="See more case studies with <?php echo esc_attr( $product_type ); ?> product type" href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-product-type<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $product_type_slug ); ?>"><?php echo esc_html( $product_type ); ?></a>
-			</div>
+				<p><span class="strong"><?php echo esc_html( $product_type_text ); ?>:</span>
+				<a
+					title="See more case studies with <?php echo esc_attr( $product_type ); ?> product type"
+					href="/case-studies<?php echo esc_attr( $url_type ); ?>?_sft_lf-product-type<?php echo esc_attr( $url_type ); ?>=<?php echo esc_attr( $product_type_slug ); ?>"><?php echo esc_html( $product_type ); ?></a>
+			</p>
 				<?php
-					endif;
+endif;
 			?>
-			<div>
-				<p><?php echo esc_html( $date_published ); ?></p>
-				<a class="skew-box secondary" href="<?php the_permalink(); ?>"><?php the_date(); ?></a>
+			<p><span class="strong"><?php echo esc_html( $date_published ); ?>:</span> <?php the_date(); ?></p>
 			</div>
 
+			<?php if ( ! empty( $projects ) && ! is_wp_error( $projects ) ) { ?>
+			<div>
+			<h5 class="margin-bottom">
+					<?php
+					if ( 'lf_case_study_cn' === get_post_type() ) {
+						echo '使用的项目';
+					} else {
+						echo 'Projects used';
+					}
+					?>
+				</h5>
+
+				<div class="case-study-project-icons">
+					<?php
+					foreach ( $projects as $project ) {
+						?>
+					<div class="case-study-project-icon">
+						<img loading="lazy"
+							src="<?php echo esc_url( get_stylesheet_directory_uri() ) . '/images/projects/' . esc_html( $project->slug ) . '-icon-color.svg'; ?>"
+							alt="<?php echo esc_html( $project->name ); ?>">
+					</div>
+						<?php
+					}
+					?>
+				</div>
+			</div>
+				<?php
+			}
+			?>
+
+<div class="case-study-subscription-block">
+			<h5 class="margin-bottom">Stay informed</h5>
+<p class="margin-bottom">Interested in reading more successful case studies from our end user community? <strong>Sign up for our newsletter.</strong></p>
+<?php echo do_shortcode( '[hubspot type=form portal=8112310 id=afe5f966-bae5-4fce-bd5d-84f7ae89111b]' ); ?>
+	<p class="privacy-agreement">See footer for our privacy policy.</p>
+			</div>
+
+			</div>
 		</div>
-	</div>
 </section>
 	<?php
 	$block_content = ob_get_clean();
